5a20d06c79b56370e03e3df1ee759a93b0cda8cd
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-09-14 18:54:29 -0400

    add packages to serve from merge

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 //Express and db connection
-const express = require('express');
-const db = require('./config/connection');
-const path = require('path');
+const express = require("express");
+const db = require("./config/connection");
+const path = require("path");
 //Apollo typeDefs and resolvers
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 //Port number and app
 const PORT = process.env.PORT || 3001;
@@ -13,9 +13,9 @@ const app = express();
 
 //Apollo server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
 
 //Apollo Middleware
@@ -27,17 +27,17 @@ app.use(express.json());
 // Include images later!!!!!!!
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
