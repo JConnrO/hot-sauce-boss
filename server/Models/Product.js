d68881afca0039387b034c05ece5e6be6ea1ceb0
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-09-14 19:40:55 -0400

    product name added to schema

--- server/Models/Product.js
+++ server/Models/Product.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    }
