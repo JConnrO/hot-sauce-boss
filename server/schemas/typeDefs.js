f3a01e299d0f968d5f9b5024c67979194859d4bc
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-09-14 19:59:59 -0400

    category and product added

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,19 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  
