3be427c6471b84e33a034c5433ae9a8c03d79ef7
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-14 20:19:39 -0400

    list style

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,12 +8,17 @@ function Nav() {
 
             <h2>
                 <a data-testid="link" href="/">
-                    <img src={headerImage} style={{ width: "10%", height: "10%" }} alt="logo" /> 
+                    <img src={headerImage} style={{ width: "10%", height: "10%" }} alt="logo" />
                 </a>
             </h2>
 
             <nav>
-            <a href="/"><img src="https://img.icons8.com/dotty/30/000000/test-account.png" alt="account" /> </a>
+                <ul>
+                    <li>
+                        {/* update href with correct link */}
+                        <a href="/"><img src="https://img.icons8.com/dotty/30/000000/test-account.png" alt="account" /> </a>
+                    </li>
+                </ul>
             </nav>
 
         </header>
