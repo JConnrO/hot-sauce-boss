b4d8b39db8bf7fbd91a3033f60f85efab9895f2a
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-09-14 20:02:21 -0400

    user, query and authentication added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,4 +16,24 @@ const typeDefs = gql`
     category: Category
   }
 
-  
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+ 
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+  }
+  `
+  module.exports = typeDefs;
