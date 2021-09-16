39478adf4cb2b140cf0b2a711360cfde738db13a
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-16 00:52:19 -0400

    target header background color

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 // import React from 'react';
-// import headerImage from "../../assets/nav/logo.png";
+import headerImage from "../../assets/nav/logo.png";
 // import './index.css';
 
 // function Nav() {
@@ -38,33 +38,44 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        color: "#FF70A6"
+    },
+    title: {
+        flexGrow: 1,
+        color: "#FF70A6"
+    },
+    coolers: {
+     backgroundColor: "#70D6FF"
+    },
+    text: {
+        color: "#FF70A6"
+    }
+
 }));
 
 export default function Nav() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    return (
+        <div className={classes.root} >
+            <AppBar position="static" className={classes.coolers}>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <img src={headerImage} style={{ width: "10%", height: "10%" }} alt="logo" />
+
+                    <Typography variant="h6" className={classes.title}>
+                        HotSauceBoss
+                    </Typography>
+                    <Button className={classes.text}>Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
