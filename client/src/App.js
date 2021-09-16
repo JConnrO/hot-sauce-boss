2c6f2360008dd6a4b8c89385210104c413b93253
jconnro <j.connor.obrien@gmail.com>
2021-09-16 19:42:48 -0400

    Removed Signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Nav from './components/Nav';
-import SignIn from './components/SignupForm';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
-      <SignIn></SignIn>
     </div>
   );
 }
