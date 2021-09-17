bbf6ef580435b71ed2840791f5f80585f8a6b996
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-16 20:39:24 -0400

    import material sign in template

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav';
-
+import SignUpForm from './components/SignupForm';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
-
+      <SignUpForm></SignUpForm>
     </div>
   );
 }
