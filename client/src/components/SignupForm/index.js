995e9833b88b951126efdfed871053f325c64b36
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-16 20:54:06 -0400

    auth

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -15,6 +15,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
 const useStyles = makeStyles((theme) => ({
     coolers: {
         color: "#70D6FF"
