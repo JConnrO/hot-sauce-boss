c361faf86d7b41f6878fa4eb7a9fc8abc42b8f3d
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-09-18 00:40:23 -0400

    all hot sauce images added

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -21,7 +21,7 @@ db.once('open', async () => {
       name: 'Hoffs Wake Up Call Hot Sauce',
       description:
         'Hoffs most complex hot sauce is handcrafted with fresh chilis, gourmet spices',
-      image: 'cookie-tin.jpg',
+      image: 'hoffs.jpeg',
       category: categories[0]._id,
       price: 5.99,
       quantity: 50
@@ -30,7 +30,7 @@ db.once('open', async () => {
       name: 'Cholula Hot Sauce',
       description:
         'This bottle was made with the every-day-every-meal hot sauce user in mind. 12 ounces of the iconic Mexican sauce allows you to be generous with your servings',
-      image: 'canned-coffee.jpg',
+      image: 'cholula.jpeg',
       category: categories[1]._id,
       price: 16.99,
       quantity: 50
@@ -40,7 +40,7 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'Mexicos number one selling hot sauce. Valentina Extra Hot Sauce is a red, pourable hot sauce from Mexico made from puya chilis and is thicker and less acidic than Tabasco with a distinct chili flavour. ',
-      image: 'toilet-paper.jpg',
+      image: 'valentina.jpeg',
       price: 8.99,
       quantity: 100
     },
@@ -49,7 +49,7 @@ db.once('open', async () => {
       category: categories[3]._id,
       description:
         'Old Chrismasy Chrismacy',
-      image: 'soap.jpg',
+      image: 'Old Christmas.jpeg',
       price: 32.00,
       quantity: 50
     },
@@ -58,7 +58,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Sriracha Hot Chili Sauce from Huy Fong. Made from sun ripened chilis',
-      image: 'wooden-spoons.jpg',
+      image: 'Tong ot sriracha.jpg',
       price: 14.99,
       quantity: 100
     },
@@ -68,7 +68,7 @@ db.once('open', async () => {
       category: categories[0]._id,
       description:
         'Franks redhot chilli hotsauce queen naija cayenne pepper',
-      image: 'camera.jpg',
+      image: 'franks.jpeg',
       price: 29.99,
       quantity: 30
     },
@@ -77,7 +77,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description:
         'This is an old favourite. Tapatios major asset is the absence of vinegar in the recipe which promises genuine pepper flavours. Have a try! learn more',
-      image: 'tablet.jpg',
+      image: 'tapatio.jpeg',
       price: 79.99,
       quantity: 50
     },
@@ -86,7 +86,7 @@ db.once('open', async () => {
       category: categories[3]._id,
       description:
         'About the Sauce: Volcanus, made with the world’s hottest pepper, Smokin’ Ed’s Carolina Reaper, this sauce packs a punch for the uninitiated. Your lips ',
-      image: 'bedtime-book.jpg',
+      image: 'inferno.jpeg',
       price: 9.99,
       quantity: 100
     },
@@ -94,7 +94,7 @@ db.once('open', async () => {
       name: 'Louisiana Hot Sauce',
       category: categories[4]._id,
       description: 'a hot sauce with a 90-year history of great taste and quality. using time-honored techniques of Louisiana style cooking, this hot sauce is produced ...',
-      image: 'spinning-top.jpg',
+      image: 'louisiana.jpeg',
       price: 8.99,
       quantity: 10
     },
@@ -103,7 +103,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description:
         'This Aubrey D. Rebel hot sauce is made with the GHOST pepper (a.k.a. JOLOKIA/NAGA) and it is the hottest natural and unmodified pepper in the world We ...',
-      image: 'plastic-horses.jpg',
+      image: 'jolokia.jpeg',
       price: 17.99,
       quantity: 1000
     } 
