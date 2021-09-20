16d854e534106b8d2c3ad0c0c0ac4a108b44807b
J. Connor O'Brien <j.connor.obrien@gmail.com>
2021-09-20 13:22:46 -0400

    Sauces Hardcoded

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -2,22 +2,89 @@
 import React from 'react';
 
 // import photo from '../../assets/small/reciplanner.jpg';
-import Card from 'react-bootstrap/Card'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Product = (props) => {
+    // input ProductInput {
+    //     name: String
+    //     description: String
+    //     image: String
+    //     quantity: Int
+    //     price: Float
+    //   }
+    const hotsauces = [
+        {
+            name: "Franks Red",
+            description: "Frank's Red Hot Sauce",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+        },
+        {
+            name: "Cholula",
+            description: "Cholula1",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+
+        },
+        {
+            name: "Tabasco",
+            description: "Tabasco1",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+        },
+        {
+            name: "Mule Sauce",
+            description: "Mule Sauce1",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+        },
+        {
+            name: "Hot Ones: The Last Dab XXX",
+            description: "Hot Ones: The Last Dab XXX1",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+        },
+        {
+            name: "Hot Ones - Los Calientes",
+            description: "Hot Ones - Los Calientes1",
+            image: "0",
+            quantity: "1",
+            price: "1.99"
+        }
+    ]
 
     return (
-        <Card style={{ width: '18rem' }} className="bg-dark text-white polaroid">
-            <Card.Img src={require(`../../assets/small/${props.image}.png`).default} alt={props.alt} />
-            <Card.ImgOverlay className="imgOverlay">
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                    {props.description}
-                </Card.Text>
-                <a href={props.github}><img src="https://img.icons8.com/material-outlined/96/000000/github.png" alt="GitHub Repo" /> </a>
-                <a href={props.deployed}><img src="https://img.icons8.com/clouds/100/000000/domain.png" alt="Live Application" /></a>
-            </Card.ImgOverlay>
-        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={hotsauces.image}
+          alt={hotsauces.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {hotsauces.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {hotsauces.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Edit</Button>
+          <Button size="small">Delete</Button>
+        </CardActions>
+      </Card>
     );
 }
 
