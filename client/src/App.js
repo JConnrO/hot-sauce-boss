38345a425d71432dd8d6f39b711b98905082862d
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-20 08:19:18 -0400

    set up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Nav from './components/Nav';
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Nav></Nav>
+      </div>
+    </ApolloProvider >
   );
 }
 
